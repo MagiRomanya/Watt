7d952777a192c669928e29eecb0a3f1d4d59f6e1
Magí Romanyà <magiromanya@gmail.com>
2025-02-28 15:42:32 +0100

    Add Spring Energies
    Signed-off-by: Magí Romanyà <magiromanya@gmail.com>

--- tests/Spring/tst_Spring.cpp
+++ tests/Spring/tst_Spring.cpp
@@ -1,2 +1,50 @@
 #include <catch2/catch_all.hpp>
-#include <watt_Spring.hpp>
+#include <TinyAD/Scalar.hh>
+
+#include "watt_Spring.hpp"
+
+TEST_CASE("SPRING ENERGY DERIVATIVES")
+{
+    using StateVec = Eigen::Vector<double, 6>;
+    StateVec x = StateVec::Zero();
+
+    constexpr auto size = StateVec::SizeAtCompileTime;
+    constexpr auto nTests = 1024;
+    constexpr auto tol = 1e-8;
+
+    auto deformation = Catch::Generators::generate("displacement generator", CATCH_INTERNAL_LINEINFO, [nTests] {
+        // NOLINTNEXTLINE
+        using namespace Catch::Generators;
+        return makeGenerators(
+            take(nTests, map([](const auto &) { return (1.0 * StateVec::Random()).eval(); }, range(0, nTests))));
+    });
+
+    x += deformation;
+
+    constexpr double L0 = 1.0;
+    constexpr double k = 1.0;
+
+    using ADouble = TinyAD::Scalar<size, double>;
+    Eigen::Vector<ADouble, 6> xAD = ADouble::make_active(x);
+
+    const ADouble energyAD = Watt::Spring::computeEnergy<ADouble>(xAD.segment<3>(0), xAD.segment<3>(3), k, L0);
+    SECTION("Energy")
+    {
+        const double energyWatt = Watt::Spring::computeEnergy<double>(x.segment<3>(0), x.segment<3>(3), k, L0);
+        REQUIRE_THAT(energyWatt - energyAD.val, Catch::Matchers::WithinAbs(0, tol));
+    }
+    SECTION("Energy Gradient")
+    {
+        const Eigen::Vector<double, 6> gradientWatt =
+            Watt::Spring::computeEnergyGradient<double>(x.segment<3>(0), x.segment<3>(3), k, L0);
+        REQUIRE_THAT((gradientWatt - energyAD.grad).norm(), Catch::Matchers::WithinAbs(0, tol));
+    }
+
+    SECTION("Energy Hessian")
+    {
+        const Eigen::Matrix<double, 6, 6> hessianWatt =
+            Watt::Spring::computeEnergyHessian<double>(x.segment<3>(0), x.segment<3>(3), k, L0);
+
+        REQUIRE_THAT((hessianWatt - energyAD.Hess).norm(), Catch::Matchers::WithinAbs(0, tol));
+    }
+}
--- watt_Spring.hpp
+++ watt_Spring.hpp
@@ -3,8 +3,46 @@
 
 #include <Eigen/Dense>
 
-namespace watt
+namespace Watt::Spring
 {
-};
+// Distinction because of TinyAD integration
+template <typename DoF, typename Parameter>
+DoF computeEnergy(const Eigen::Vector3<DoF> &xA, const Eigen::Vector3<DoF> &xB, Parameter k, Parameter L0)
+{
+    DoF L = (xA - xB).norm();
+    return 0.5 * k * (L - L0) * (L - L0);
+}
+
+template <typename Scalar>
+Eigen::Vector<Scalar, 6> computeEnergyGradient(const Eigen::Vector3<Scalar> &xA,
+                                               const Eigen::Vector3<Scalar> &xB,
+                                               Scalar k,
+                                               Scalar L0)
+{
+    Eigen::Vector<Scalar, 6> gradient;
+    Scalar L = (xA - xB).norm();
+    gradient.template segment<3>(0) = k * (L - L0) * (xA - xB) / L;
+    gradient.template segment<3>(3) = -gradient.template segment<3>(0);
+    return gradient;
+}
+
+template <typename Scalar>
+Eigen::Matrix<Scalar, 6, 6> computeEnergyHessian(const Eigen::Vector3<Scalar> &xA,
+                                                 const Eigen::Vector3<Scalar> &xB,
+                                                 Scalar k,
+                                                 Scalar L0)
+{
+    Eigen::Matrix<Scalar, 6, 6> hessian;
+    Scalar L = (xA - xB).norm();
+    const Eigen::Vector3<Scalar> u = (xA - xB) / L;
+    hessian.template block<3, 3>(0, 0) =
+        k / L * ((L - L0) * Eigen::Matrix3<Scalar>::Identity() + L0 * u * u.transpose());
+    hessian.template block<3, 3>(0, 3) = -hessian.template block<3, 3>(0, 0);
+    hessian.template block<3, 3>(3, 0) = -hessian.template block<3, 3>(0, 0);
+    hessian.template block<3, 3>(3, 3) = hessian.template block<3, 3>(0, 0);
+    return hessian;
+}
+
+};  // namespace Watt::Spring
 
 #endif  // WATT_SPRING_H_
